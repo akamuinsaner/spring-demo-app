4d6a988068ef2ac039df1260c68842e747f217f0
王帅 <wangshuai602708@pwrd.com>
2023-05-05 11:40:32 +0800

    fix

--- src/main/java/com/example/demo/model/sample/Sample.java
+++ src/main/java/com/example/demo/model/sample/Sample.java
@@ -3,14 +3,20 @@ import com.example.demo.constant.CommonEnums;
 import com.example.demo.model.Auditable;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
+import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "sample")
 @Data
-public class Sample extends Auditable<String> {
+public class Sample {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -89,5 +95,17 @@ public class Sample extends Auditable<String> {
 
     @Transient
     private String tip;
+
+    @Column(name = "created_by")
+    protected String createdBy;
+
+    @Column(name = "created_date")
+    protected Date createdDate;
+
+    @Column(name = "last_modified_by")
+    protected String lastModifiedBy;
+
+    @Column(name = "last_modified_date")
+    protected Date lastModifiedDate;
 
 }
